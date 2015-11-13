2e8dacb7c80542ee1f54335ecd8c5e0eb3fbd462
Tran Huy Cong <congtranit@gmail.com>
2015-11-13 17:25:06 +0700

    UI

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 navigator.webkitGetUserMedia({ video: false, audio: true }, function (stream) {
-  var Peer = require('simple-peer')
+  var Peer = require('simple-peer');
   var peer = new Peer({
     initiator: location.hash === '#init',
     trickle: false,
     stream: stream
-  })
+  });
 
   peer.on('signal', function (data) {
     document.getElementById('yourId').value = JSON.stringify(data)
-  })
+  });
 
   document.getElementById('connect').addEventListener('click', function () {
-    var otherId = JSON.parse(document.getElementById('otherId').value)
+    var otherId = JSON.parse(document.getElementById('otherId').value);
     peer.signal(otherId)
-  })
+  });
 
   document.getElementById('send').addEventListener('click', function () {
-    var yourMessage = document.getElementById('yourMessage').value
-    peer.send(yourMessage)
-  })
+    var yourMessage = document.getElementById('yourMessage').value;
+    peer.send(yourMessage);
+    document.getElementById('yourMessage').value="";
+    document.getElementById('messages').textContent += "Me:"+yourMessage + '\n';
+
+  });
 
   peer.on('data', function (data) {
-    document.getElementById('messages').textContent += data + '\n'
-  })
+    document.getElementById('messages').textContent += "Friend: "+data + '\n';
+  });
   //
   // peer.on('stream', function (stream) {
   //   var video = document.createElement('video')
@@ -33,4 +36,4 @@ navigator.webkitGetUserMedia({ video: false, audio: true }, function (stream) {
   // })
 }, function (err) {
   console.error(err)
-})
+});
